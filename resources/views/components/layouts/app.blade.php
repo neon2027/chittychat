7ef32d20bc25a1b93137f963bec1ef10d2202ccb
Exequiel Lustan <lustan.tikel20@gmail.com>
2024-12-28 20:01:26 +0800

    Update page title to use application name from configuration

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name') }}</title>
     @filamentScripts
     @vite(['resources\js\app.js', 'resources\css\app.css'])
     <style>
